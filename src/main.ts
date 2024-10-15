c10b310529e535af4ddc86a080e5dc4d737d53c5
jowihall <148017202+jjoshall@users.noreply.github.com>
2024-10-15 15:47:38 -0700

    Step 1 Complete

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,18 @@
 import "./style.css";
 
-const APP_NAME = "HELLO I AM JOSH";
+const APP_NAME = "This is the app title";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
+// Create and append the title element
+const titleElement = document.createElement("h1");
+titleElement.textContent = APP_NAME;
+app.appendChild(titleElement);
+
+// Adding a canvas to webpage of size 256x256
+const canvas = document.createElement("canvas");
+canvas.width = 256;
+canvas.height = 256;
+app.appendChild(canvas);
+
+// Set the document title
 document.title = APP_NAME;
-app.innerHTML = APP_NAME;
